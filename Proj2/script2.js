d7eeb0e18cb492cbf9ba9ab30c0ee5456f1f04f9
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-02-28 21:22:04 -0500

    Working on proj2 arrays

--- Proj2/script2.js
+++ Proj2/script2.js
@@ -4,6 +4,7 @@ document.write(fruits.length + " ");
 
 
 //Split string into array
-var things = "Apples, Oranges, Squares, Circles"
+var things = "Apples, Oranges, Squares, Circles";
 things = things.split(",");
+things[1] = "Square!";
 document.write(things + " " + things.length + " " + things[0]);
