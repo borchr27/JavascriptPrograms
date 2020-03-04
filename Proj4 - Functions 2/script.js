967fbc1d9668cd3a5bda39098952d02fc593474b
borchr27 <30349201+borchr27@users.noreply.github.com>
2020-03-04 09:25:57 -0500

    More JS practice, grabbing elements

---
+++ Proj4 - Functions 2/script.js
@@ -0,0 +1,10 @@
+var header = document.getElementById("header");
+header.innerHTML = "Mitchells New H1";
+header.style.color = "red";
+header.style.backgroundColor = "purple";
+
+
+var link = document.getElementById("link");
+link.innerHTML = "Link to Google.com";
+link.style.color = "brown";
+link.style.backgroundColor = "green";
